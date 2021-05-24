0834919fb9bd6eadec619d22f4dc87712a4342be
Jocelin HOUNON <hounonj@gmail.com>
2021-05-24 10:53:07 +0100

    cities and sistrict

--- decoupe_benin.py
+++ decoupe_benin.py
@@ -52,8 +52,44 @@ class Departement:
         return f"Dep({self.id};{self.label}"
 
 
+class District:
+    def __init__(self, _id, label):
+        self.id = _id
+        self.name = label
+        self.enabled = True
+
+    def toJSON(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'enabled': True
+        }
+
+    def __repr__(self):
+        return f"Dist({self.id};{self.label}"
+class City:
+    def __init__(self, _id, label):
+        self.id = _id
+        self.name = label
+        self.districts = []
+
+    def add_district(self, district: District):
+        self.districts.append(district)
+
+    def toJSON(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'enabled': True,
+            'districts': [d.toJSON() for d in self.districts]
+        }
+
+    def __repr__(self):
+        return f"Com({self.id};{self.label}"
+
 departements = []
 quartiers = []
+cities = []
 with open('benin.json') as f:
     data = json.load(f)
     for _departement in data:
@@ -61,22 +97,23 @@ with open('benin.json') as f:
             _departement["id_dep"], _departement["lib_dep"])
         for _commune in _departement["communes"]:
             commune = Commune(_commune["id_com"], _commune["lib_com"])
+            city = City(_commune["id_com"], _commune["lib_com"])
             for _arrond in _commune["arrondissements"]:
                 arrondissement = Arrondissement(
                     _arrond['id_arrond'], _arrond['lib_arrond'])
                 for _quartier in _arrond["quartiers"]:
-                    quartier = Quartier(_quartier['id_quart'],_quartier['lib_quart'])
+                    quartier = Quartier(
+                        _quartier['id_quart'], _quartier['lib_quart'])
+                    district = District(
+                        _quartier['id_quart'], _quartier['lib_quart'])
+                    city.add_district(district)
                     arrondissement.add_quartier(quartier)
                     if commune.id == 48:
                         quartiers.append(quartier)
                 commune.add_arrond(arrondissement)
             departement.add_commune(commune)
+            cities.append(city.toJSON())
         departements.append(departement)
-    print(departements[7].communes[0].arronds[5].quartiers)
 
-#######Manage Quartier#####
-with open("cotonou_q.data","w") as qf:
-    qs = []
-    for quartier in quartiers:
-        qs.append(quartier.fileString()+"\n")
-    qf.writelines(qs)
+with open('cities.json', 'w') as f:
+    json.dump(cities, f)
